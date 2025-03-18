054634cf3e83b28c5c3d568329b72f5db1f3a1aa
Fatih <fatakhillah.khaqo@gmail.com>
2025-03-18 14:14:39 +0700

    Initial Commit

---
+++ PokeProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeProfile
+//
+//  Created by Fatakhillah Khaqo on 18/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
